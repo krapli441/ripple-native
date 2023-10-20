dfebb4fe43ff3b7325fdd461fc026a82728ad1de
박준형 <108733795+krapli441@users.noreply.github.com>
2023-10-20 15:44:06 +0900

    맵 스타일 분리

--- components/MapScreen.tsx
+++ components/MapScreen.tsx
@@ -1,221 +1,51 @@
 // React & React Native
-import React from 'react';
-import {
-  SafeAreaView,
-  StatusBar,
-  ScrollView,
-  View,
-  Text,
-  TouchableOpacity,
-  StyleSheet,
-  Image,
-  useColorScheme,
-} from 'react-native';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import React, {useState, useEffect} from 'react';
+import {View, StyleSheet, useColorScheme} from 'react-native';
 // Others
-import Icon from 'react-native-vector-icons/FontAwesome';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import mapStyle from '../maps/customMapStyle.json';
+import Geolocation from '@react-native-community/geolocation';
 
 function MapScreen(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
-
   const containerStyle = {
     ...styles.container,
-    backgroundColor: isDarkMode ? 'black' : 'white',
+    backgroundColor: isDarkMode ? 'black' : 'wshite',
   };
+
+  const [region, setRegion] = useState({
+    latitude: 37.78825,
+    longitude: -122.4324,
+    latitudeDelta: 0.015,
+    longitudeDelta: 0.0121,
+  });
+  useEffect(() => {
+    const watchId = Geolocation.watchPosition(
+      position => {
+        const {latitude, longitude} = position.coords;
+        setRegion({
+          ...region,
+          latitude,
+          longitude,
+        });
+      },
+      error => console.log(error),
+      {
+        enableHighAccuracy: true,
+        timeout: 20000,
+        maximumAge: 1000,
+        distanceFilter: 10,
+      },
+    );
+
+    return () => Geolocation.clearWatch(watchId);
+  }, []);
 
   return (
     <View style={styles.container}>
       <MapView
         provider={PROVIDER_GOOGLE}
-        customMapStyle={[
-          {
-            elementType: 'geometry',
-            stylers: [
-              {
-                color: '#212121',
-              },
-            ],
-          },
-          {
-            elementType: 'labels.icon',
-            stylers: [
-              {
-                visibility: 'off',
-              },
-            ],
-          },
-          {
-            elementType: 'labels.text.fill',
-            stylers: [
-              {
-                color: '#757575',
-              },
-            ],
-          },
-          {
-            elementType: 'labels.text.stroke',
-            stylers: [
-              {
-                color: '#212121',
-              },
-            ],
-          },
-          {
-            featureType: 'administrative',
-            elementType: 'geometry',
-            stylers: [
-              {
-                color: '#757575',
-              },
-            ],
-          },
-          {
-            featureType: 'administrative.country',
-            elementType: 'labels.text.fill',
-            stylers: [
-              {
-                color: '#9e9e9e',
-              },
-            ],
-          },
-          {
-            featureType: 'administrative.land_parcel',
-            stylers: [
-              {
-                visibility: 'off',
-              },
-            ],
-          },
-          {
-            featureType: 'administrative.locality',
-            elementType: 'labels.text.fill',
-            stylers: [
-              {
-                color: '#bdbdbd',
-              },
-              {
-                visibility: 'on',
-              },
-            ],
-          },
-          {
-            featureType: 'poi',
-            elementType: 'labels.text.fill',
-            stylers: [
-              {
-                color: '#757575',
-              },
-            ],
-          },
-          {
-            featureType: 'poi.park',
-            elementType: 'geometry',
-            stylers: [
-              {
-                color: '#181818',
-              },
-            ],
-          },
-          {
-            featureType: 'poi.park',
-            elementType: 'labels.text.fill',
-            stylers: [
-              {
-                color: '#616161',
-              },
-            ],
-          },
-          {
-            featureType: 'poi.park',
-            elementType: 'labels.text.stroke',
-            stylers: [
-              {
-                color: '#1b1b1b',
-              },
-            ],
-          },
-          {
-            featureType: 'road',
-            elementType: 'geometry.fill',
-            stylers: [
-              {
-                color: '#2c2c2c',
-              },
-            ],
-          },
-          {
-            featureType: 'road',
-            elementType: 'labels.text.fill',
-            stylers: [
-              {
-                color: '#8a8a8a',
-              },
-            ],
-          },
-          {
-            featureType: 'road.arterial',
-            elementType: 'geometry',
-            stylers: [
-              {
-                color: '#373737',
-              },
-            ],
-          },
-          {
-            featureType: 'road.highway',
-            elementType: 'geometry',
-            stylers: [
-              {
-                color: '#3c3c3c',
-              },
-            ],
-          },
-          {
-            featureType: 'road.highway.controlled_access',
-            elementType: 'geometry',
-            stylers: [
-              {
-                color: '#4e4e4e',
-              },
-            ],
-          },
-          {
-            featureType: 'road.local',
-            elementType: 'labels.text.fill',
-            stylers: [
-              {
-                color: '#616161',
-              },
-            ],
-          },
-          {
-            featureType: 'transit',
-            elementType: 'labels.text.fill',
-            stylers: [
-              {
-                color: '#757575',
-              },
-            ],
-          },
-          {
-            featureType: 'water',
-            elementType: 'geometry',
-            stylers: [
-              {
-                color: '#000000',
-              },
-            ],
-          },
-          {
-            featureType: 'water',
-            elementType: 'labels.text.fill',
-            stylers: [
-              {
-                color: '#3d3d3d',
-              },
-            ],
-          },
-        ]}
+        customMapStyle={mapStyle}
         style={styles.map}
         region={{
           latitude: 37.78825,
