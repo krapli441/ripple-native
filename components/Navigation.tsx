7993d0630f87a39e97ca9ad5995d780439c77407
박준형 <108733795+krapli441@users.noreply.github.com>
2023-10-23 19:11:48 +0900

    아이콘 크기 조절

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -21,19 +21,19 @@ const CustomTabBar = () => {
       <TouchableOpacity
         style={styles.button}
         onPress={() => navigation.navigate('Home')}>
-        <Icon name="home" size={30} />
+        <Icon name="home" size={25} />
         <Text style={styles.textStyle}>홈</Text>
       </TouchableOpacity>
       <TouchableOpacity
         style={styles.button}
         onPress={() => navigation.navigate('Ripple')}>
-        <Icon name="map" size={30} />
+        <Icon name="map" size={25} />
         <Text style={styles.textStyle}>지도</Text>
       </TouchableOpacity>
       <TouchableOpacity
         style={styles.button}
         onPress={() => navigation.navigate('Profile')}>
-        <Icon name="user" size={30} />
+        <Icon name="user" size={25} />
         <Text style={styles.textStyle}>내 정보</Text>
       </TouchableOpacity>
     </View>
