b0f2428620ca6f00ab6423d43a04faa93cea528a
박준형 <108733795+krapli441@users.noreply.github.com>
2023-10-24 17:46:19 +0900

    마커 생성 기본 로직까지 작성

--- components/MapScreen.tsx
+++ components/MapScreen.tsx
@@ -32,6 +32,30 @@ type PositionData = {
 
 function MapScreen(): React.ReactElement {
   const isDarkMode = useColorScheme() === 'dark';
+  const [coords, setCoords] = useState<Coords | null>(null);
+  const [region, setRegion] = useState<Region | null>(null);
+
+  useEffect(() => {
+    Geolocation.getCurrentPosition(
+      position => {
+        const {latitude, longitude} = position.coords;
+        setCoords({latitude, longitude});
+
+        // region도 업데이트
+        setRegion({
+          latitude,
+          longitude,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+        });
+      },
+      error => {
+        console.log(error);
+      },
+      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000},
+    );
+  }, []);
+
   return (
     <View style={styles.container}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
@@ -39,7 +63,7 @@ function MapScreen(): React.ReactElement {
         provider={PROVIDER_GOOGLE}
         customMapStyle={MapStyle}
         style={styles.map}
-        // region={region}
+        region={region || undefined}
         mapPadding={{bottom: 90, top: 0, right: 0, left: 0}}
         scrollEnabled={false}
         zoomEnabled={true}
@@ -50,7 +74,7 @@ function MapScreen(): React.ReactElement {
         pitchEnabled={false}
         cacheEnabled={true}
         loadingEnabled={true}>
-        {/* <Marker.Animated coordinate={coords} title="Your Position" /> */}
+        {coords && <Marker coordinate={coords} title="Your Position" />}
       </MapView>
       <CustomTabBar />
     </View>
