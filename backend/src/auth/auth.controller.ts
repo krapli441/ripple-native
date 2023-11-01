9babdd358c3f5d854dab8bd7d275d4236e52816b
박준형 <108733795+krapli441@users.noreply.github.com>
2023-11-01 17:05:29 +0900

    코드 작성하였지만 실패, 원인 찾아야 함

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+// auth.controller.ts
+import { Controller, Get, UseGuards, Request } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  @Get('spotify')
+  @UseGuards(AuthGuard('spotify'))
+  async spotifyAuth() {}
+
+  @Get('spotify/callback')
+  @UseGuards(AuthGuard('spotify'))
+  async spotifyAuthCallback(@Request() req) {
+    return req.user;
+  }
+}
