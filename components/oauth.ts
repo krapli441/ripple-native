2ad0e40749804c96cdbbc1ca701debc354bcc77e
박준형 <108733795+krapli441@users.noreply.github.com>
2023-11-01 17:32:07 +0900

    로직 수정, nest가 url을 잘 생성해주는지 확인 필요함

--- components/oauth.ts
+++ components/oauth.ts
@@ -1,9 +1,14 @@
 const handleSpotifyLogin = async () => {
   try {
-    // NestJS 서버의 '/auth/spotify' 엔드포인트로 요청 보냄.
+    // NestJS 서버의 '/auth/spotify' 엔드포인트로 요청을 보냄
     const response = await fetch('http://192.168.0.215:3000/auth/spotify');
-    const data = await response.json();
-    console.log(data);
+    if (!response.ok) {
+      console.log('Network response was not ok', response.status);
+      return;
+    }
+
+    // 이 부분에서 웹 브라우저나 웹 뷰를 열어서 사용자가 인증할 수 있도록 처리
+    // 예: Linking.openURL(response.url);
   } catch (error) {
     console.log(error);
   }
