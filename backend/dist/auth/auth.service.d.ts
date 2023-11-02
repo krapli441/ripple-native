1923a14cc17c0bf80f1f8076b41a8d653840c57b
박준형 <108733795+krapli441@users.noreply.github.com>
2023-11-02 16:53:09 +0900

    express-session 설치

--- backend/dist/auth/auth.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { JwtService } from '@nestjs/jwt';
-export declare class AuthService {
-    private jwtService;
-    constructor(jwtService: JwtService);
-    validateSpotifyToken(accessToken: string): Promise<boolean>;
-    createJwt(user: any): string;
-    createRefreshToken(): string;
-}
--- backend/dist/auth/spotify-oauth2.strategy.d.ts
+++
@@ -1,8 +0,0 @@
-import { ConfigService } from '@nestjs/config';
-declare const SpotifyOAuth2Strategy_base: new (...args: any[]) => any;
-export declare class SpotifyOAuth2Strategy extends SpotifyOAuth2Strategy_base {
-    private configService;
-    constructor(configService: ConfigService);
-    validate(accessToken: string, refreshToken: string, profile: any, req: any): Promise<any>;
-}
-export {};
