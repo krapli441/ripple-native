e69253e7ec78b774f2ccfee8b1080a4da6399eb0
박준형 <108733795+krapli441@users.noreply.github.com>
2023-11-02 11:26:59 +0900

    passport 초기화 후 테스트, 반응 없음.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as passport from 'passport';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Passport 초기화
+  app.use(passport.initialize());
+
   await app.listen(3000);
 }
 bootstrap();
