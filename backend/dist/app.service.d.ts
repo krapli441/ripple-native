7be76adcf2b507d7cb6caf1d46e72206d671da37
박준형 <108733795+krapli441@users.noreply.github.com>
2023-11-02 10:41:22 +0900

    NestJS 삭제, 다시 시도해보자

--- backend/dist/app.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { ConfigService } from '@nestjs/config';
-export declare class AppService {
-    getHello(): string;
-}
-export declare class MyService {
-    private configService;
-    constructor(configService: ConfigService);
-    someMethod(): void;
-}
