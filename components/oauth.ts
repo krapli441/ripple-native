e96998a0e365d2633d5e12c526ae1cfe3539bc01
박준형 <108733795+krapli441@users.noreply.github.com>
2023-11-03 14:29:34 +0900

    auth를 사용하여 로그인 시도 - 클라이언트 로직 수정

--- components/oauth.ts
+++ components/oauth.ts
@@ -2,16 +2,21 @@ import {authorize, AuthConfiguration} from 'react-native-app-auth';
 import Config from 'react-native-config';
 import {Linking} from 'react-native';
 
+const config: AuthConfiguration = {
+  clientId: Config.SPOTIFY_CLIENT_ID!,
+  redirectUrl: 'http://192.168.0.215:3000/auth/spotify/',
+  scopes: ['user-read-email', 'playlist-modify-public', 'user-read-private'],
+  serviceConfiguration: {
+    authorizationEndpoint: 'https://accounts.spotify.com/authorize',
+    tokenEndpoint: 'http://192.168.0.215:3000/auth/spotify/token', // 여기를 NestJS 서버로 지정합니다.
+  },
+};
+
 const handleSpotifyLogin = async () => {
   try {
-    // NestJS 서버로 로그인 요청을 보냄
-    const response = await fetch('http://192.168.0.215:3000/auth/spotify');
-    const data = await response.json();
-
-    // 인증 URL로 사용자를 리다이렉트
-    if (data.authorizeUrl) {
-      Linking.openURL(data.authorizeUrl);
-    }
+    const result = await authorize(config);
+    // 여기에서 result에는 인증 코드 정보가 포함되어 있다.
+    // 이 정보를 서버에 전달하여 액세스 토큰을 받아와야 한다.
   } catch (error) {
     console.error('Error during login:', error);
   }
