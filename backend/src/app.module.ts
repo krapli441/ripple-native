c0fc540e9e5a846a503b05f93e1771ae2b540801
박준형 <108733795+krapli441@users.noreply.github.com>
2023-11-03 10:37:39 +0900

    mongodb 주소 추가

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { AuthModule } from './auth/auth.module';
       isGlobal: true,
     }),
     AuthModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/ripple'),
   ],
   controllers: [AppController],
   providers: [AppService],
