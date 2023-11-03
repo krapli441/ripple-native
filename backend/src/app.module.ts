805b87e79f92b67fc583065faa15a6ab52557425
박준형 <108733795+krapli441@users.noreply.github.com>
2023-11-03 12:17:09 +0900

    app.module.ts에 pasport 추가

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [],
+  imports: [PassportModule.register({ defaultStrategy: 'spotify' })],
   controllers: [AppController],
   providers: [AppService],
 })
