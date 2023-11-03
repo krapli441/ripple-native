9c3e479dade8a87b5ad242956cdb8e6b5f33877e
박준형 <108733795+krapli441@users.noreply.github.com>
2023-11-03 12:19:19 +0900

    spotify-strategy.service.ts 생성

--- backend/src/auth/spotify-strategy/spotify-strategy.service.ts
+++ backend/src/auth/spotify-strategy/spotify-strategy.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-spotify';
 
 @Injectable()
-export class SpotifyStrategyService {}
+export class SpotifyStrategyService extends PassportStrategy(
+  Strategy,
+  'spotify',
+) {
+  constructor() {
+    super({
+      clientID: process.env.SPOTIFY_CLIENT_ID,
+      clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+      callbackURL: 'http://localhost:3000/auth/spotify/callback',
+      scope: ['user-read-email', 'playlist-modify-public', 'user-read-private'],
+    });
+  }
+
+  async validate(accessToken: string, refreshToken: string, profile: any) {
+    // 여기에서 사용자 정보를 DB와 비교하거나 JWT를 생성할 수 있음.
+    return profile;
+  }
+}
