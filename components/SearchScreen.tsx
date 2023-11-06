d0030ab1398026c4ea6d49add1c860e3b2c5d406
박준형 <108733795+krapli441@users.noreply.github.com>
2023-11-06 15:23:27 +0900

    키보드를 숨기는 함수 추가

--- components/SearchScreen.tsx
+++ components/SearchScreen.tsx
@@ -1,43 +1,37 @@
 // React & React Native
-import React, {useEffect, useState, useRef} from 'react';
+import React from 'react';
 import {
   View,
-  Animated,
   StatusBar,
-  useColorScheme,
-  AppState,
-  TouchableOpacity,
-  TouchableWithoutFeedback,
-  Easing,
   Text,
   TextInput,
-  Image,
+  TouchableWithoutFeedback,
+  Keyboard,
+  useColorScheme,
 } from 'react-native';
-// Libraries
-import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
-
-// Components
-
-// Util
-
-// Types
-
 // Style
 import styles from '../styles/SearchScreenStyle';
 
 function SearchScreen(): React.ReactElement {
   const isDarkMode = useColorScheme() === 'dark';
 
+  // 키보드를 숨기는 함수
+  const dismissKeyboard = () => {
+    Keyboard.dismiss();
+  };
+
   return (
-    <View style={styles.searchContainer}>
-      <StatusBar barStyle={isDarkMode ? 'dark-content' : 'dark-content'} />
-      <Text style={styles.header}>음악 남기기</Text>
-      <TextInput
-        style={styles.inputStyle}
-        placeholder="음악을 검색해주세요"
-        placeholderTextColor={isDarkMode ? 'grey' : 'darkgrey'}
-      />
-    </View>
+    <TouchableWithoutFeedback onPress={dismissKeyboard}>
+      <View style={styles.searchContainer}>
+        <StatusBar barStyle={isDarkMode ? 'dark-content' : 'dark-content'} />
+        <Text style={styles.header}>음악 남기기</Text>
+        <TextInput
+          style={styles.inputStyle}
+          placeholder="음악을 검색해주세요"
+          placeholderTextColor={isDarkMode ? 'grey' : 'darkgrey'}
+        />
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
