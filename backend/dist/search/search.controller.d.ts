f9d451c0558a38cfd20fb0d832d54fea4330e791
박준형 <108733795+krapli441@users.noreply.github.com>
2023-11-06 17:47:15 +0900

    디버깅이 필요할듯함

--- backend/dist/search/search.controller.d.ts
+++ backend/dist/search/search.controller.d.ts
@@ -1,2 +1,7 @@
+import { SearchService } from './search.service';
+import { Request } from 'express';
 export declare class SearchController {
+    private readonly searchService;
+    constructor(searchService: SearchService);
+    searchMusic(query: string, req: Request): Promise<any>;
 }
--- backend/src/search/search.controller.ts
+++ backend/src/search/search.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards, Get, Query, Req } from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard'; // 경로는 실제 경로로 변경해야 합니다.
+import { SearchService } from './search.service';
+import { Request } from 'express';
 
 @Controller('search')
-export class SearchController {}
+export class SearchController {
+  constructor(private readonly searchService: SearchService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Get()
+  async searchMusic(@Query('query') query: string, @Req() req: Request) {
+    // JwtAuthGuard를 통과하면, req 객체에 사용자 정보가 포함된다.
+    const userId = req.body; // 사용자 ID를 가져옴
+    try {
+      const results = await this.searchService.searchMusicForUser(
+        userId,
+        query,
+      );
+      return results;
+    } catch (error) {
+      // 적절한 예외 처리
+      throw new Error(error.message);
+    }
+  }
+}
