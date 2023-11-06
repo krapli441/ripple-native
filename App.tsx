ef7185e3b1c5f086a1bc05cd764e8472cf69c99a
박준형 <108733795+krapli441@users.noreply.github.com>
2023-11-06 13:04:53 +0900

    이 방식은 아닌 듯하다

--- App.tsx
+++ App.tsx
@@ -8,30 +8,32 @@ import {createStackNavigator} from '@react-navigation/stack';
 
 // Components
 import HomeScreen from './components/HomeScreen';
-import MapScreen from './components/MapScreen';
-import SearchScreen from './components/SearchScreen';
-import NavigationTabBar from './components/Navigation';
+// import LibraryScreen from './components/LibraryScreen';
+// import ProfileScreen from './components/ProfileScreen';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
+// 로그인 후 보여질 하단 탭 네비게이터
+function MainTabNavigator() {
+  return (
+    <Tab.Navigator>
+      {/* <Tab.Screen name="Library" component={LibraryScreen} /> */}
+      <Tab.Screen name="Home" component={HomeScreen} />
+      {/* <Tab.Screen name="Profile" component={ProfileScreen} /> */}
+    </Tab.Navigator>
+  );
+}
+
 function App(): JSX.Element {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator>
+        {/* 로그인 스크린 등 초기 스크린을 여기에 배치합니다 */}
+        {/* 로그인 이후에 메인 탭으로 이동 */}
         <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name="Ripple"
-          component={MapScreen}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name="SearchScreen"
-          component={SearchScreen}
+          name="Main"
+          component={MainTabNavigator}
           options={{headerShown: false}}
         />
       </Stack.Navigator>
