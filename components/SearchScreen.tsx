0dd23256e04345dc6d2f226ab56a645e132ad08c
박준형 <108733795+krapli441@users.noreply.github.com>
2023-11-06 19:12:02 +0900

    음악 재생 완료

--- components/SearchScreen.tsx
+++ components/SearchScreen.tsx
@@ -1,4 +1,3 @@
-// React & React Native
 import React, {useState} from 'react';
 import {
   View,
@@ -9,6 +8,10 @@ import {
   Keyboard,
   useColorScheme,
   Button,
+  FlatList,
+  Image,
+  Linking,
+  TouchableOpacity,
 } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
@@ -17,46 +20,52 @@ import {useFocusEffect} from '@react-navigation/native';
 // Style
 import styles from '../styles/SearchScreenStyle';
 
-const searchForMusic = async (searchQuery: string) => {
-  try {
-    // AsyncStorage에서 JWT 토큰을 가져옴
-    const jwtToken = await AsyncStorage.getItem('userToken');
-
-    if (!jwtToken) {
-      throw new Error('사용자 인증 토큰이 없습니다.');
-    }
-
-    // 백엔드 엔드포인트로 검색 요청을 보낸다.
-    const response = await fetch('http://172.30.1.27:3000/search', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${jwtToken}`, // JWT 토큰을 Authorization 헤더에 포함
-      },
-      body: JSON.stringify({query: searchQuery}),
-    });
-
-    const data = await response.json();
-
-    if (response.ok) {
-      // 검색 결과를 처리
-      console.log('검색 결과:', data);
-    } else {
-      // 서버에서 에러 메시지를 받은 경우
-      throw new Error(data.message || '검색을 완료할 수 없습니다.');
-    }
-  } catch (error) {
-    console.error('검색 중 오류 발생:', error);
-  }
-};
-
 function SearchScreen(): React.ReactElement {
   const isDarkMode = useColorScheme() === 'dark';
-  const [searchTerm, setSearchTerm] = useState(''); // 검색어 상태를 추가
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
 
-  const hadleSearchMusic = () => {
+  const searchForMusic = async (searchQuery: string) => {
+    try {
+      // AsyncStorage에서 JWT 토큰을 가져옴
+      const jwtToken = await AsyncStorage.getItem('userToken');
+
+      if (!jwtToken) {
+        throw new Error('사용자 인증 토큰이 없습니다.');
+      }
+
+      // 백엔드 엔드포인트로 검색 요청을 보낸다.
+      const response = await fetch('http://172.30.1.27:3000/search', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${jwtToken}`,
+        },
+        body: JSON.stringify({query: searchQuery}),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        const tracks = data.map((item: any) => ({
+          title: item.name,
+          artist: item.artists.map((artist: any) => artist.name).join(', '),
+          externalUrl: item.external_urls.spotify,
+          imageUrl: item.album.images[0].url, // 가장 큰 이미지를 선택합니다.
+        }));
+        setSearchResults(tracks);
+      } else {
+        throw new Error(data.message || '검색을 완료할 수 없습니다.');
+      }
+    } catch (error) {
+      console.error('검색 중 오류 발생:', error);
+    }
+  };
+
+  const handleSearchMusic = () => {
     searchForMusic(searchTerm);
   };
+
   const handleSearchTermChange = (text: string) => {
     setSearchTerm(text);
   };
@@ -69,17 +78,28 @@ function SearchScreen(): React.ReactElement {
   useFocusEffect(
     React.useCallback(() => {
       StatusBar.setBarStyle('dark-content');
-      return () => {
-        // 여기도 마찬가지로 다른 스크린으로 이동할 때의 상태 표시줄 스타일을 복구하는 데 사용할 수 있다.
-      };
+      return () => {};
     }, []),
+  );
+
+  const renderItem = ({item}: any) => (
+    <View style={styles.resultItem}>
+      <Image source={{uri: item.imageUrl}} style={styles.albumCover} />
+      <View style={styles.infoContainer}>
+        <Text style={styles.title}>{item.title}</Text>
+        <Text style={styles.artist}>{item.artist}</Text>
+      </View>
+      <TouchableOpacity onPress={() => Linking.openURL(item.externalUrl)}>
+        <Text style={styles.link}>Play</Text>
+      </TouchableOpacity>
+    </View>
   );
 
   return (
     <TouchableWithoutFeedback onPress={dismissKeyboard}>
       <View style={styles.searchContainer}>
-        <StatusBar barStyle={isDarkMode ? 'dark-content' : 'dark-content'} />
-        <Text style={styles.header}>음악 남기기</Text>
+        <StatusBar barStyle={isDarkMode ? 'dark-content' : 'light-content'} />
+        <Text style={styles.header}>음악 검색</Text>
         <TextInput
           style={styles.inputStyle}
           placeholder="음악을 검색해주세요"
@@ -87,7 +107,12 @@ function SearchScreen(): React.ReactElement {
           value={searchTerm}
           placeholderTextColor={isDarkMode ? 'grey' : 'darkgrey'}
         />
-        <Button title="검색" onPress={hadleSearchMusic} />
+        <Button title="검색" onPress={handleSearchMusic} />
+        <FlatList
+          data={searchResults}
+          renderItem={renderItem}
+          keyExtractor={(item, index) => `result-${index}`}
+        />
       </View>
     </TouchableWithoutFeedback>
   );
--- styles/SearchScreenStyle.ts
+++ styles/SearchScreenStyle.ts
@@ -22,6 +22,35 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     fontSize: 18,
   },
+  resultItem: {
+    flexDirection: 'row', // 아이템을 수평으로 배치합니다.
+    marginVertical: 10, // 상하 여백을 줍니다.
+    alignItems: 'center', // 아이템을 중앙에 배치합니다.
+    marginLeft: 20, // 왼쪽 여백을 줍니다.
+  },
+  albumCover: {
+    width: 60, // 이미지의 너비를 설정합니다.
+    height: 60, // 이미지의 높이를 설정합니다.
+    borderRadius: 30, // 이미지를 원형으로 만듭니다.
+    marginRight: 20, // 오른쪽 여백을 줍니다.
+  },
+  infoContainer: {
+    flex: 1, // 나머지 공간을 모두 사용합니다.
+    justifyContent: 'center', // 중앙에 배치합니다.
+  },
+  title: {
+    fontSize: 16, // 제목의 글자 크기를 설정합니다.
+    fontWeight: 'bold', // 글자를 굵게 합니다.
+  },
+  artist: {
+    fontSize: 14, // 아티스트 정보의 글자 크기를 설정합니다.
+    color: 'gray', // 글자 색을 회색으로 합니다.
+  },
+  link: {
+    fontSize: 14, // 링크의 글자 크기를 설정합니다.
+    color: 'blue', // 글자 색을 파란색으로 합니다.
+    marginRight: 20, // 오른쪽 여백을 줍니다.
+  },
 });
 
 export default styles;
