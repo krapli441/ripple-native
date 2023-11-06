e841656aefddd35478496a51cb969fba03a605d6
박준형 <108733795+krapli441@users.noreply.github.com>
2023-11-06 13:20:19 +0900

    수정이 많이 필요하지만 우선 이동 완료

--- App.tsx
+++ App.tsx
@@ -8,6 +8,8 @@ import {createStackNavigator} from '@react-navigation/stack';
 
 // Components
 import HomeScreen from './components/HomeScreen';
+import NavigationTabBar from './components/Navigation';
+import MapScreen from './components/MapScreen';
 // import LibraryScreen from './components/LibraryScreen';
 // import ProfileScreen from './components/ProfileScreen';
 
@@ -17,9 +19,13 @@ const Tab = createBottomTabNavigator();
 // 로그인 후 보여질 하단 탭 네비게이터
 function MainTabNavigator() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator tabBar={props => <NavigationTabBar {...props} />}>
       {/* <Tab.Screen name="Library" component={LibraryScreen} /> */}
-      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen
+        name="Home"
+        component={MapScreen}
+        options={{headerShown: false}}
+      />
       {/* <Tab.Screen name="Profile" component={ProfileScreen} /> */}
     </Tab.Navigator>
   );
@@ -29,10 +35,15 @@ function App(): JSX.Element {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        {/* 로그인 스크린 등 초기 스크린을 여기에 배치합니다 */}
+        {/* 로그인 스크린을 여기에 배치 */}
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{headerShown: false}}
+        />
         {/* 로그인 이후에 메인 탭으로 이동 */}
         <Stack.Screen
-          name="Main"
+          name="Ripple"
           component={MainTabNavigator}
           options={{headerShown: false}}
         />
--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -5,14 +5,58 @@ import {BottomTabBarProps} from '@react-navigation/bottom-tabs';
 
 import styles from '../styles/NavigationStyles';
 
+// 아이콘 이름은 예시입니다. 실제 아이콘 이름과 일치해야 합니다.
+const ICONS = {
+  Library: 'book',
+  Home: 'home',
+  Profile: 'user',
+};
+
 const NavigationTabBar: React.FC<BottomTabBarProps> = ({
   state,
   descriptors,
   navigation,
 }) => {
-  return <View style={styles.tabContainer}>
-    
-  </View>;
+  return (
+    <View style={styles.tabContainer}>
+      {state.routes.map((route, index) => {
+        const {options} = descriptors[route.key];
+        const label =
+          options.tabBarLabel !== undefined
+            ? options.tabBarLabel
+            : options.title !== undefined
+            ? options.title
+            : route.name;
+
+        const isFocused = state.index === index;
+
+        const onPress = () => {
+          const event = navigation.emit({
+            type: 'tabPress',
+            target: route.key,
+            canPreventDefault: true,
+          });
+
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name);
+          }
+        };
+
+        const iconName = ICONS[route.name];
+
+        return (
+          <TouchableOpacity
+            key={route.key}
+            onPress={onPress}
+            style={styles.tabItem}
+          >
+            <Icon name={iconName} size={20} color={isFocused ? 'tomato' : 'gray'} />
+            <Text style={styles.tabLabel}>{label}</Text>
+          </TouchableOpacity>
+        );
+      })}
+    </View>
+  );
 };
 
 export default NavigationTabBar;
