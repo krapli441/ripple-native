0ee1d5a197cda5383704c744e2b498056ee33b56
박준형 <108733795+krapli441@users.noreply.github.com>
2023-11-08 15:52:01 +0900

    SearchTagScreen 추가

--- components/SearchTagScreen.tsx
+++ components/SearchTagScreen.tsx
@@ -1,5 +1,5 @@
 // react & react-native
-import React, {useState, useLayoutEffect} from 'react';
+import React, {useState, useLayoutEffect, useEffect} from 'react';
 import {
   View,
   StatusBar,
@@ -8,11 +8,8 @@ import {
   TouchableWithoutFeedback,
   Keyboard,
   useColorScheme,
-  FlatList,
   KeyboardAvoidingView,
   Platform,
-  Image,
-  Alert,
 } from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import type {NavigationProp} from '@react-navigation/native';
@@ -22,18 +19,63 @@ import {useFocusEffect} from '@react-navigation/native';
 import {RootStackParamList} from '../types/navigationTypes';
 import {TrackDetails} from '../types/navigationTypes';
 
-// asyncStorage
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
 // Style
-import styles from '../styles/SearchScreenStyle';
+import styles from '../styles/SearchTagScrenStyle';
 import {TouchableOpacity} from 'react-native-gesture-handler';
+
+interface Tag {
+  name: string;
+}
 
 function SearchTagScreen(): React.ReactElement {
   const isDarkMode = useColorScheme() === 'dark';
   const [searchTerm, setSearchTerm] = useState('');
   const [searchResults, setSearchResults] = useState([]);
   const navigation = useNavigation<NavigationProp<RootStackParamList>>();
+  const [tags, setTags] = useState<Tag[]>([]);
+  const [selectedTags, setSelectedTags] = useState<string[]>([]);
+
+  useEffect(() => {
+    const fetchTags = async () => {
+      try {
+        const response = await fetch('http://192.168.0.215:3000/tags/random');
+        if (!response.ok) {
+          throw new Error('Server error');
+        }
+        const data: Tag[] = await response.json();
+        setTags(data); // 응답으로 받은 데이터를 상태에 저장
+        console.log(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchTags();
+  }, []);
+
+  const toggleTag = (tagName: any) => {
+    setSelectedTags(prevSelectedTags => {
+      if (prevSelectedTags.includes(tagName)) {
+        return prevSelectedTags.filter(tag => tag !== tagName); // 태그 해제
+      } else {
+        return [...prevSelectedTags, tagName]; // 태그 선택
+      }
+    });
+  };
+
+  const getTagStyle = (tagName: any) => {
+    const isSelected = selectedTags.includes(tagName);
+    return {
+      backgroundColor: isSelected ? 'black' : 'grey',
+      color: isSelected ? 'white' : 'black',
+    };
+  };
+
+  const getTagTextStyle = (tagName: string) => {
+    const isSelected = selectedTags.includes(tagName);
+    return {
+      color: isSelected ? 'white' : 'black',
+    };
+  };
 
   // 화면 포커스 시 실행될 이펙트
   useFocusEffect(
@@ -60,26 +102,6 @@ function SearchTagScreen(): React.ReactElement {
       return () => {};
     }, []),
   );
-
-  const goToSearchTagScreen = () => {
-    navigation.navigate('SearchTagScreen');
-  };
-
-  // const handleSelectTrack = (track: TrackDetails) => {
-  //   navigation.navigate('MakeRippleScreen', {track});
-  // };
-
-  // const renderItem = ({item}: any) => (
-  //   <TouchableOpacity onPress={() => handleSelectTrack(item)}>
-  //     <View style={styles.resultItem}>
-  //       <Image source={{uri: item.imageUrl}} style={styles.albumCover} />
-  //       <View style={styles.infoContainer}>
-  //         <Text style={styles.title}>{item.title}</Text>
-  //         <Text style={styles.artist}>{item.artist}</Text>
-  //       </View>
-  //     </View>
-  //   </TouchableOpacity>
-  // );
 
   return (
     <KeyboardAvoidingView
--- styles/SearchTagScrenStyle.ts
+++ styles/SearchTagScrenStyle.ts
@@ -5,6 +5,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#f8f9fa',
     justifyContent: 'flex-start',
   },
+
+  tagsContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    padding: 25,
+    paddingTop: 10,
+    paddingBottom: 5,
+  },
+
   header: {
     fontSize: 32,
     fontWeight: 'bold',
@@ -13,7 +24,21 @@ const styles = StyleSheet.create({
     marginLeft: 30,
   },
   flexContainer: {
-    flex: 1, 
+    flex: 1,
+  },
+  tagText: {
+    textAlign: 'center',
+    fontSize: 16,
+  },
+  tag: {
+    height: 40,
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 20,
+    margin: 5,
+    marginHorizontal: 4, // 좌우 마진
+    marginBottom: 4,
   },
 
   inputStyle: {
@@ -26,10 +51,10 @@ const styles = StyleSheet.create({
   },
   resultItem: {
     flexDirection: 'row',
-    marginVertical: 10, 
-    alignItems: 'center', 
-    marginLeft: 30, 
-    marginRight: 20, 
+    marginVertical: 10,
+    alignItems: 'center',
+    marginLeft: 30,
+    marginRight: 20,
   },
   flatListStyle: {
     flex: 1,
@@ -39,22 +64,22 @@ const styles = StyleSheet.create({
     paddingBottom: 120,
   },
   albumCover: {
-    width: 60, 
-    height: 60, 
+    width: 60,
+    height: 60,
     borderRadius: 5,
     marginRight: 20,
   },
   infoContainer: {
-    flex: 1, 
+    flex: 1,
     justifyContent: 'center',
   },
   title: {
-    fontSize: 16, 
-    fontWeight: 'bold', 
+    fontSize: 16,
+    fontWeight: 'bold',
   },
   artist: {
-    fontSize: 14, 
-    color: 'gray', 
+    fontSize: 14,
+    color: 'gray',
   },
   link: {
     fontSize: 14,
