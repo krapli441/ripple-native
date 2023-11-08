699258051151291aa830e8791ec5479beb7008a9
박준형 <108733795+krapli441@users.noreply.github.com>
2023-11-08 12:14:36 +0900

    테스트 시작

--- backend/dist/music-tag/music-tag.controller.d.ts
+++
@@ -1,6 +0,0 @@
-import { TagService } from './music-tag.service';
-export declare class TagController {
-    private readonly tagService;
-    constructor(tagService: TagService);
-    getRandomTags(): Promise<import("./tag.schema").Tag[]>;
-}
--- backend/dist/music-tag/music-tag.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { Model } from 'mongoose';
-import { Tag } from './tag.schema';
-export declare class TagService {
-    private tagModel;
-    constructor(tagModel: Model<Tag>);
-    getRandomTags(): Promise<Tag[]>;
-}
