8a54693d490d19aaab0ab755a22154c94d798adc
박준형 <108733795+krapli441@users.noreply.github.com>
2023-11-14 14:59:32 +0900

    아예 최소 코드만 보이도록 변경

--- components/LibraryScreen.tsx
+++ components/LibraryScreen.tsx
@@ -1,48 +1,18 @@
 // react & react-native
 import React, {useState} from 'react';
-import {
-  View,
-  StatusBar,
-  Text,
-  TextInput,
-  TouchableWithoutFeedback,
-  Keyboard,
-  useColorScheme,
-  FlatList,
-  KeyboardAvoidingView,
-  Platform,
-  Image,
-  Alert,
-} from 'react-native';
-import {useNavigation} from '@react-navigation/native';
-import type {NavigationProp} from '@react-navigation/native';
-import {useFocusEffect} from '@react-navigation/native';
-
-// types
-import {RootStackParamList} from '../types/navigationTypes';
-import {TrackDetails} from '../types/navigationTypes';
-
-// asyncStorage
-import useAuthToken from '../utils/useAuthToken';
+import {View, StatusBar, Text, useColorScheme} from 'react-native';
 
 // Style
 import styles from '../styles/LibraryScreenStyles';
-import {TouchableOpacity} from 'react-native-gesture-handler';
 
 function LibraryScreen(): React.ReactElement {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
-    <KeyboardAvoidingView
-      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-      style={styles.flexContainer}>
-      <TouchableWithoutFeedback>
-        <View style={styles.searchContainer}>
-          <StatusBar barStyle={isDarkMode ? 'dark-content' : 'light-content'} />
-          <Text style={styles.header}>라이브러리</Text>
-        </View>
-      </TouchableWithoutFeedback>
-    </KeyboardAvoidingView>
+    <View style={styles.searchContainer}>
+      <StatusBar barStyle={isDarkMode ? 'dark-content' : 'light-content'} />
+      <Text style={styles.header}>라이브러리</Text>
+    </View>
   );
 }
 
