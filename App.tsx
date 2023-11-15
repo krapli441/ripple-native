9afe3086b40d4f57c1b75b4efc07b3c037000626
박준형 <108733795+krapli441@users.noreply.github.com>
2023-11-15 16:48:32 +0900

    알림 뜨는거 확인함

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 // React & React Native
-import React from 'react';
+import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 
 // Libraries
@@ -22,6 +22,9 @@ import LikedRippleScreen from './components/LikedRippleScreen';
 // AuthProvider
 import AuthProvider from './utils/AuthContext';
 import {LocationProvider} from './utils/LocationContext';
+
+// Notification
+import messaging from '@react-native-firebase/messaging';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -129,6 +132,21 @@ function MainTabNavigator() {
 }
 
 function App(): JSX.Element {
+  async function requestUserPermission() {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+    if (enabled) {
+      console.log('Authorization status:', authStatus);
+    }
+  }
+
+  useEffect(() => {
+    requestUserPermission();
+  }, []);
+
   return (
     <AuthProvider>
       <LocationProvider>
