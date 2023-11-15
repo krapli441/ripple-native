d0a2b209633e848bca77611ab423746b4e431dd6
박준형 <108733795+krapli441@users.noreply.github.com>
2023-11-15 13:35:13 +0900

    appdeletage 수정

--- ios/ripple/AppDelegate.mm
+++ ios/ripple/AppDelegate.mm
@@ -2,36 +2,35 @@
 #import <GoogleMaps/GoogleMaps.h>
 #import "RNCConfig.h"
 #import <React/RCTBundleURLProvider.h>
+#import <Firebase.h> // Firebase 라이브러리 임포트
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [FIRApp configure];
+
   NSString *googleMapsApiKey = [RNCConfig envFor:@"GOOGLE_API_KEY"];
   [GMSServices provideAPIKey:googleMapsApiKey];
 
-  
-
   self.moduleName = @"ripple";
-  self.initialProps = @{};
+  self.initialProps = {};
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
- - (BOOL) application: (UIApplication *) application
- continueUserActivity: (nonnull NSUserActivity *)userActivity
-   restorationHandler: (nonnull void (^)(NSArray<id<UIUserActivityRestoring>> * _Nullable))restorationHandler
-  {
-    if ([userActivity.activityType isEqualToString:NSUserActivityTypeBrowsingWeb]) {
-      if (self.authorizationFlowManagerDelegate) {
-        BOOL resumableAuth = [self.authorizationFlowManagerDelegate resumeExternalUserAgentFlowWithURL:userActivity.webpageURL];
-        if (resumableAuth) {
-          return YES;
-        }
+- (BOOL)application:(UIApplication *)application continueUserActivity:(nonnull NSUserActivity *)userActivity restorationHandler:(nonnull void (^)(NSArray<id<UIUserActivityRestoring>> * _Nullable))restorationHandler
+{
+  if ([userActivity.activityType isEqualToString:NSUserActivityTypeBrowsingWeb]) {
+    if (self.authorizationFlowManagerDelegate) {
+      BOOL resumableAuth = [self.authorizationFlowManagerDelegate resumeExternalUserAgentFlowWithURL:userActivity.webpageURL];
+      if (resumableAuth) {
+        return YES;
       }
     }
-    return [RCTLinkingManager application:application continueUserActivity:userActivity restorationHandler:restorationHandler];
   }
+  return [RCTLinkingManager application:application continueUserActivity:userActivity restorationHandler:restorationHandler];
+}
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
