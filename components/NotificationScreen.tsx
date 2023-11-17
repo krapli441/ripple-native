ce727e38cf0b96b24c46ee2f175d5554bd44ca88
박준형 <108733795+krapli441@users.noreply.github.com>
2023-11-17 15:01:25 +0900

    상태바 보이도록 변경

--- components/NotificationScreen.tsx
+++ components/NotificationScreen.tsx
@@ -32,12 +32,17 @@ import {TouchableOpacity} from 'react-native-gesture-handler';
 function NotificationScreen(): React.ReactElement {
   const isDarkMode = useColorScheme() === 'dark';
   const navigation = useNavigation<NavigationProp<RootStackParamList>>();
-  const authToken = useAuthToken();
-
   // 키보드를 숨기는 함수
   const dismissKeyboard = () => {
     Keyboard.dismiss();
   };
+
+  useFocusEffect(
+    React.useCallback(() => {
+      StatusBar.setBarStyle('dark-content');
+      return () => {};
+    }, []),
+  );
 
   return (
     <TouchableWithoutFeedback onPress={dismissKeyboard}>
