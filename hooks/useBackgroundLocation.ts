4b5798d587d12e523de017d8696134071dfc3474
박준형 <108733795+krapli441@users.noreply.github.com>
2023-11-27 12:27:23 +0900

    코드 수정 전 커밋

--- hooks/useBackgroundLocation.ts
+++ hooks/useBackgroundLocation.ts
@@ -9,19 +9,32 @@ import BackgroundGeolocation, {
 const useBackgroundLocation = () => {
   useEffect(() => {
     // 위치 서비스 구성
-    BackgroundGeolocation.ready({
-      desiredAccuracy: BackgroundGeolocation.DESIRED_ACCURACY_HIGH,
-      distanceFilter: 10,
-      stopOnTerminate: false,
-      startOnBoot: true,
-      debug: true,
-      logLevel: BackgroundGeolocation.LOG_LEVEL_VERBOSE,
-    }, (state) => {
-      console.log('[BackgroundGeolocation] ready: ', state);
-      if (!state.enabled) {
-        BackgroundGeolocation.start();
-      }
-    });
+    BackgroundGeolocation.ready(
+      {
+        locationAuthorizationRequest: 'Always',
+        locationAuthorizationAlert: {
+          titleWhenNotEnabled: '위치 서비스가 필요합니다',
+          titleWhenOff: '위치 서비스가 꺼져 있습니다',
+          instructions:
+            "앱 설정으로 이동하여 위치 접근 권한을 '항상'으로 설정해주세요.",
+          cancelButton: '취소',
+          settingsButton: '설정',
+        },
+        desiredAccuracy: BackgroundGeolocation.DESIRED_ACCURACY_HIGH,
+        distanceFilter: 0,
+        heartbeatInterval: 5,
+        stopOnTerminate: false,
+        startOnBoot: true,
+        debug: true,
+        logLevel: BackgroundGeolocation.LOG_LEVEL_VERBOSE,
+      },
+      state => {
+        console.log('[BackgroundGeolocation] ready: ', state);
+        if (!state.enabled) {
+          BackgroundGeolocation.start();
+        }
+      },
+    );
 
     // 위치 업데이트 리스너 등록
     BackgroundGeolocation.onLocation(
@@ -30,7 +43,7 @@ const useBackgroundLocation = () => {
       },
       (error: LocationError) => {
         console.warn('[Location Error] ', error);
-      }
+      },
     );
 
     // HTTP 리스너 등록
